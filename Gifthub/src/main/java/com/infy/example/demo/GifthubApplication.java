d2c53cdc1e6408a21aa7caf53cb808b00db0ef65
shaik.jaheer <shaik.jaheer@HYDPCM580211L.ad.infosys.com>
2022-06-14 16:21:22 +0530

    commit

---
+++ Gifthub/src/main/java/com/infy/example/demo/GifthubApplication.java
@@ -0,0 +1,14 @@
+package com.infy.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifthubApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ Gifthub/src/test/java/com/infy/example/demo/GifthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
